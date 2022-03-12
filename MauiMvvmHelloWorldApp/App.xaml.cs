1a466e1a2d77bb7784046e12f042e300f479bc72
pierlam-project <pierlam-project@outlook.com>
2022-03-12 15:48:21 +0100

    first commit of the initial default MAUI project source code

---
+++ MauiMvvmHelloWorldApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiMvvmHelloWorldApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiMvvmHelloWorldApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiMvvmHelloWorldApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
